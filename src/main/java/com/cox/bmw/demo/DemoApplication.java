f80b2b10296c5df342619a64f30ad74ce160c5d0
Sharma, Sudhakar (CCI-Atlanta-CON) <Sudhakar.Sharma@cox.com>
2017-08-30 14:35:42 +0530

    Added resouce and docker maven plugin in pom.xml

--- src/main/java/com/cox/bmw/demo/DemoApplication.java
+++ src/main/java/com/cox/bmw/demo/DemoApplication.java
@@ -2,11 +2,22 @@ package com.cox.bmw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/rest")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello()
+	{
+		return "Hello User!!";
 	}
 }
